f7535832e8cd2ff27b2eeaaad75034565f04d524
Luskan101 <luskan101@gmail.com>
2024-06-18 18:57:05 -0400

    preguntas
    son preguntas que me salieron al intentar llenar la informacion como me mostraste en clases, en una de esas estoy hablando puras weas
    Pd: las lineas borradas no cacho si estaban de antes o se me autocompletaron solas con una cuestion que tuve que instalar para otra clase: "tabine"

--- core/models.py
+++ core/models.py
@@ -33,30 +33,33 @@ class Ticket(models.Model):
 # Modelo de Datos Area 
 class Area(models.Model):
     nombre_area = models.CharField(max_length=255, blank=False)
+    #oye crsitian, estaba viendo los datos que teniamos antes y no emaparece anda con vista, lo mas cercanos seria "departamento"
+    #pero viendo la linea 23 muestra como "ejecutivo telefonico"entonces seria el area que acepta el ticket, el que lo envia o alguna wea asi?
+    #y lo que tenemos marcado como "departamento" tendria solo "id_departamento" y "nombre", habria que ponerle alguna cuestion ams aparte de eso?
     
-    def __str__(self):
-        return self.nombre_area
 
 
 # Modelo de datos Tipo
 class Tipo(models.Model):
     tipo_ticket = models.CharField(max_length=50, blank=False)
-    
-    def __str__(self) :
-        return self.tipo_ticket
-    
+    #aca esta marcado en la linea 24 como "solicitud", si mal no recuerdo anotamos 4 estados para los ticket consulta, felicitacion, reclamo y problema 
+
+
+
     
 # Modelo de Datos Criticidad
 class Criticidad(models.Model):
     nombre_criticidad = models.CharField(max_length=255, blank=False)
+    #aca esta marcada en la linea 25 como "media", creo que eran 3, en este caso habria que asociarles un valor numerico a cada una(onda, 1, 2 y 3?)
+    #y aca en criticidad habria que crear 3 lineas dandoles el valor a cada una?
+    #ademasn, en la info que teniamos antes se llama "prioridad", con 2 valores:"id_prioridad" y "prioridad"
+
     
-    def __str__(self):
-        return self.nombre_criticidad
-
-
 # Modelo de datos EstadoTicket
 class Estado(models.Model):
     estado_ticket = models.CharField(max_length=55, blank=False)
-    
-    def __str__(self):
-        return self.estado_ticket
+    #aca ya estan marcados los estados con valores, encontre el valor "1" y el "4" en views.py(lineas 18, 19 y 49)
+    #tambien lo que tenemos guardado marca este con 2 datos,"id_estado" y "estado"
+
+
+
