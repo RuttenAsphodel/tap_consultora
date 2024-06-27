7ab7fa6e258f9d6158898b58bf353b5fd26231c0
Cristian Andres Valenzuela <117678691+RuttenAsphodel@users.noreply.github.com>
2024-06-27 09:56:14 -0400

    1

--- login/settings.py
+++ login/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0t%eq8mox7xtk71p02b03dgh3=bz&cvum73zx**^4gbvts+z(1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ['tap-consultora.azurewebsites.net']
 ALLOWED_HOSTS = ['10.13.99.82','127.0.0.1', 'web-3ie7hubrqz59.up-de-fra1-k8s-1.apps.run-on-seenode.com']
