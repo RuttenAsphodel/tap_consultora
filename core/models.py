bfa7518f69781774c53efd12f3bd110d97ef6100
Cristian Andres Valenzuela <tc.cvalenzuela@gmail.com>
2024-07-04 22:27:07 -0400

    cambio en str en modelo Profile

--- core/models.py
+++ core/models.py
@@ -106,9 +106,6 @@ class Profile(models.Model):
     
     
 
-    def __str__(self):
-        return self.user
-
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
