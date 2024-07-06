baa28bdd0ad97640bb2d0f454a32899a48307e41
Cristian Andres Valenzuela <117678691+RuttenAsphodel@users.noreply.github.com>
2024-07-06 12:47:33 -0400

    update varias

--- core/filters.py
+++ core/filters.py
@@ -1,14 +1,24 @@
 import django_filters
 from .models import Ticket
+from django import forms
+from datetime import datetime
 
 class TicketFilter(django_filters.FilterSet):
+
+    fecha_creacion = django_filters.DateFilter(
+        field_name='fecha_creacion', 
+        lookup_expr='exact', 
+        initial=datetime.now().date()
+    )
+
+
     class Meta:
         model = Ticket
         fields = {
             'fecha_creacion': ['exact'],
         }
-
-
+    
+             
 class FiltroEjecutivo(django_filters.FilterSet):
     class Meta:
         model = Ticket
